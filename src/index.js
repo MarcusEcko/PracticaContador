bde212c0af68c69633c016161cf31b1e4ea0fb32
MarcusEcko <marco237.mnm@gmail.com>
2025-08-02 22:54:00 -0300

    +5 +10 and reset added to function

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import { useState } from 'react';
 export default function Counter() {
   const [value, setValue] = useState(0);
 
-  const handleClick = () => (
-    setValue(prev => prev + 1)
-  )
+  const changeValue = (amount) => {
+    setValue(prev => {
+      if(amount === 0) return 0;
+      const newValue = prev + amount;
+      return newValue < 0 ? 0 : newValue;
+    });
+  }
 
   return (
     <div>
       <span>{value}</span><br/>
-      <button onClick={handleClick}>Incrementar</button>
+      <button onClick={() => changeValue(-1)}>Decrementar - 1</button>
+      <button onClick={() => changeValue(-10)}> -10 </button>
+      <button onClick={() => changeValue(-5)}> -5 </button>
+      <button onClick={() => changeValue(0)}> 0 </button>
+      <button onClick={() => changeValue(5)}> +5 </button>
+      <button onClick={() => changeValue(10)}> +10 </button>
+      <button onClick={() => changeValue(1)}>Incrementar + 1</button>
     </div>
   )
 }
