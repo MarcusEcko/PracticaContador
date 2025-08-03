0bed04048466a7bea566aac40d3cab64f9a3b72f
MarcusEcko <marco237.mnm@gmail.com>
2025-08-02 22:35:24 -0300

    basic increment added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+
+export default function Counter() {
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => (
+    setValue(prev => prev + 1)
+  )
+
+  return (
+    <div>
+      <span>{value}</span><br/>
+      <button onClick={handleClick}>Incrementar</button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
